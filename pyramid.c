a39de42a007eab3a641d4462eb56c74adde9617b
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-06-10 18:24:52 -0500

    Add files via upload

---
+++ pyramid.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main() {
+    int num;
+    scanf("%d", &num);
+    char pyr[] = "";
+    for (int i = 1; i < num+1; i++) {
+        strcat(pyr, "*");
+        printf("%s\n", pyr);
+        
+    }
+    
+}
