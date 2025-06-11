838fe5effd6a89ca4ed240be2e6697c59eb40e79
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-06-11 11:34:37 -0500

    Create pyramid.go

---
+++ left/pyramid.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+    var num int
+    var block string
+    
+    fmt.Println("Number: ")
+    fmt.Scanln(&num)    
+    for i := 1; i < num+1; i++ {
+        block += "*"
+        fmt.Println(block)
+    }
+}
