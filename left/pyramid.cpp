7a0d7b41ae8f89b580f0657ebc9e8b79372d7f5a
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-06-11 09:46:14 -0500

    Create pyramid.cpp

---
+++ left/pyramid.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+int main() {
+    int n = 0;
+    std::string block = "";
+    
+    std::cout << "Number: ";
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        block = block + "*";
+        std::cout << block + "\n";
+    }
+}
