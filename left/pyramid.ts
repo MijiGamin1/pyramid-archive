17f9bc0aa84b16f87994ab1ea0b1f94d9f885e46
Orionband <98924210+Orionband@users.noreply.github.com>
2025-06-11 13:36:15 -0500

    Create pyramid.ts ... ts? TS PMO

---
+++ left/pyramid.ts
@@ -0,0 +1,7 @@
+//Type Script => ts => ts pmo icl => icl ts pmo sm n sb rn ngl, r u srsly srs n fr rn vro? Smh lol atp js go 💔... b fr vro, idek nm, brb gng gtg atm lmao, bt ts pmo 2 js lmk lol onb fr, ac nvm b wt istg ts vro keys🙏💔 ts pmo 
+var userInput = prompt('Enter the number:');
+var block ="";
+for (let i = 0; i< 3; i++) {
+    block = block+ "*"
+    console.log(block)
+}
