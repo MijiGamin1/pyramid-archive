06f3b1fed102246575284b3fce2a4c8e46339608
realmayo1 <syedfarhanwarsi@gmail.com>
2025-06-11 17:55:34 -0500

    Create pyramid.pl
    Perl

---
+++ left/pyramid.pl
@@ -0,0 +1,13 @@
+print "Enter height of the pyramid: ";
+
+$height = <STDIN>;
+chomp $height;
+
+for($i = 1; $i <= $height; $i++)
+{
+    for($j = 1; $j <= $i; $j++)
+    {
+        print "*";
+    }
+    print "\n";
+}
