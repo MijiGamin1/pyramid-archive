6c3281dfb1ad00596f0a4a788547427b6dd2308b
realmayo1 <syedfarhanwarsi@gmail.com>
2025-06-11 09:53:38 -0500

    Create pyramid.f95
    fortran

---
+++ left/pyramid.f95
@@ -0,0 +1,15 @@
+Program Pyramid
+
+INTEGER :: h,i,j
+
+PRINT *,"Enter height of pyramid: "
+READ(*,*) h
+
+DO i = 1, h
+    DO j = 1, i
+        WRITE (*,fmt='(a)',advance='no') "*"
+    END DO 
+    WRITE (*,fmt='(a)')
+END DO  
+
+End Program Pyramid
