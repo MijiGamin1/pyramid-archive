0a8658940a4a4c79885dc7cd963af0dd06cabbba
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-06-11 10:59:29 -0500

    Create pyramid.rs

---
+++ left/pyramid.rs
@@ -0,0 +1,18 @@
+use std::io;
+fn main() {
+    let mut n = String::new();
+    println!("Number: ");
+    io::stdin().read_line(&mut n);
+    let trin = n.trim();
+    match trin.parse::<i32>() {
+        Ok(i) => pyr(i),
+        Err(..) => println!("you did it wrong"),
+    };
+} 
+fn pyr(i: i32) {
+    let mut block = String::new();
+    for j in 1..i+1 {
+        block = block + "*";
+        println!("{}", block);
+    }
+}
