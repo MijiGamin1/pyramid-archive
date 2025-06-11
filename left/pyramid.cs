15e480b4dc8d6d5d15d66f6d8643cada73c4eabc
realmayo1 <syedfarhanwarsi@gmail.com>
2025-06-11 11:05:22 -0500

    Create pyramid.cs
    C#

---
+++ left/pyramid.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Pyramid
+{
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter height of pyramid: ");
+        int h = int.Parse(Console.ReadLine());
+        for(int i = 1; i <= h; i++)
+        {
+            for(int j = 1; j <= i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
