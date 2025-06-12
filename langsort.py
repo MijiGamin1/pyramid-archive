73fee26bffcd0342bd40d2125f9867a152fbf007
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-06-12 14:19:33 -0500

    haskell

--- langsort.py
+++ langsort.py
@@ -1,5 +1,5 @@
 #Add new language here and interpret it for new list to add to README.md
-lang = ["R", "Assembly", "QBASIC (QB64)", "Batch", "Brainfuck", "C", "COBOL", "C++", "C#", "D", "Fortran 95", "Go", "Groovy", "HTML/Javascript", "Java", "Kotlin", "Lua", "Pascal", "PHP", "Perl", "PowerShell", "Python", "Q#", "Ruby", "Rust", "Scratch", "Scala", "Shell", "SPWN", "Swift", "TypeScript", "Visual Basic", "OCaml"]
+lang = ["R", "Assembly", "QBASIC (QB64)", "Batch", "Brainfuck", "C", "COBOL", "C++", "C#", "D", "Fortran 95", "Go", "Groovy", "HTML/Javascript", "Java", "Kotlin", "Lua", "Pascal", "PHP", "Perl", "PowerShell", "Python", "Q#", "Ruby", "Rust", "Scratch", "Scala", "Shell", "SPWN", "Swift", "TypeScript", "Visual Basic", "OCaml", "Haskell"]
 lang = sorted(lang)
 for i in range(len(lang)):
     print(str(i+1) + ". " + lang[i])
