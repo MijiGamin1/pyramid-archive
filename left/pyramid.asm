68a9bd8395c1c0913ebbeb2d526fb19f2c0f51ed
Orionband <98924210+Orionband@users.noreply.github.com>
2025-06-12 13:20:30 -0500

    Update pyramid.asm

--- left/pyramid.asm
+++ left/pyramid.asm
@@ -1,13 +1,16 @@
 DEFAULT REL
 
 section .data
-    prompt_msg  db "Number: ", 0
-    scan_fmt    db "%d", 0
-    newline_char db 0xA, 0
+    prompt_msg      db "Number: ", 0
+    scan_fmt        db "%d", 0
+    newline_char    db 0xA, 0
+    pause_msg       db "Press ENTER to exit...", 0
+    pause_fmt       db "%c", 0
 
 section .bss
-    input_num   resd 1
-    block_buffer resb 82
+    input_num       resd 1
+    block_buffer    resb 82
+    dummy_char      resb 1
 
 section .text
     extern printf
@@ -21,9 +24,11 @@ main:
     mov     rbp, rsp
     sub     rsp, 32
 
+    ; Print prompt
     lea     rcx, [prompt_msg]
     call    printf
 
+    ; Read integer
     lea     rdx, [input_num]
     lea     rcx, [scan_fmt]
     call    scanf
@@ -57,6 +62,19 @@ _for_loop_start:
     jmp     _for_loop_start
 
 _for_loop_end:
+flush_input_buffer:
+    lea     rdx, [dummy_char]
+    lea     rcx, [pause_fmt]      
+    call    scanf                 
+    cmp     byte [dummy_char], 0xA 
+    jne     flush_input_buffer    
+    lea     rcx, [pause_msg]
+    call    printf
+
+    lea     rdx, [dummy_char]
+    lea     rcx, [pause_fmt]
+    call    scanf                 ; asked implementation
+
     xor     rcx, rcx
     call    _proper_exit
 
