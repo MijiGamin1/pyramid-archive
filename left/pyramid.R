3abba3a51cbfe799d67d96950757fcd61beb51f1
realmayo1 <syedfarhanwarsi@gmail.com>
2025-06-11 22:25:13 -0500

    r

---
+++ left/pyramid.R
@@ -0,0 +1,10 @@
+cat("Enter height of the pyramid: ")
+height <- as.integer(readline());
+
+for(i in 1:height) {
+  line <- ""
+  for(j in 1:i) {
+    line <- paste0(line, "*")
+  }
+  print(line)
+}
