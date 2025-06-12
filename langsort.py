169eb67fe8b3c8698e1656c942d634838d175c38
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-06-12 13:35:56 -0500

    Create langsort.py
    Sorts languages alphabetically for README.md, just add in new language to lang list when committing.

---
+++ langsort.py
@@ -0,0 +1,4 @@
+lang = ["R", "Assembly", "QBASIC (QB64)", "Batch", "Brainfuck", "C", "COBOL", "C++", "C#", "D", "Fortran 95", "Go", "Groovy", "HTML/Javascript", "Java", "Kotlin", "Lua", "Pascal", "PHP", "Perl", "PowerShell", "Python", "Q#", "Ruby", "Rust", "Scratch", "Scala", "Shell", "SPWN", "Swift", "TypeScript", "Visual Basic"]
+lang = sorted(lang)
+for i in range(len(lang)):
+    print(str(i+1) + ": " + lang[i])
