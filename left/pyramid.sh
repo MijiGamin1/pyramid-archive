05657a04a1683f48baae097026e1b4a39a9a2aa1
realmayo1 <syedfarhanwarsi@gmail.com>
2025-06-11 20:24:45 -0500

    Update pyramid.sh

--- left/pyramid.sh
+++ left/pyramid.sh
@@ -1,6 +1,5 @@
 #i dont use linux, so this might not work on all distros
 #i used ParrotOS on a different computer to make this
-#also it uses o's instead of asterisks because asterisks pulled up something file related
 
 read -p "Num: " num
 for i in $(seq 1 $num)
