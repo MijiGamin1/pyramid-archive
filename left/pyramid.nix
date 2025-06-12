9bddec7c056af0c1823c282e7335a2e6ff74f4d4
ClemTheAlien <ClemTheAlien@proton.me>
2025-06-11 22:29:01 -0500

    Nix added in the shortest amount of code

---
+++ left/pyramid.nix
@@ -0,0 +1,13 @@
+#run with nix installed with `nix-shell` pyramid.nix
+
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p bash
+read -p "Num: " num
+for i in $(seq 1 $num)
+do
+    for j in $(seq 1 $i)
+    do  
+        printf "*";
+    done
+    echo "";
+done
