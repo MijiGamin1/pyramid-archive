ed64b98d70124c35dbfcc2fd1a35bddccfd80fa7
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-06-12 14:47:27 -0500

    fixed it

--- langsort.py
+++ langsort.py
@@ -1,5 +1,10 @@
 #Add new language here and interpret it for new list to add to README.md
 lang = ["R", "Assembly", "QBASIC (QB64)", "Batch", "Brainfuck", "C", "COBOL", "C++", "C#", "D", "Fortran 95", "Go", "Groovy", "HTML/Javascript", "Java", "Kotlin", "Lua", "Pascal", "PHP", "Perl", "PowerShell", "Python", "Q#", "Ruby", "Rust", "Scratch", "Scala", "Shell", "SPWN", "Swift", "TypeScript", "Visual Basic", "OCaml", "Haskell"]
-lang = sorted(lang)
+upperlang = []
+ul2 = []
 for i in range(len(lang)):
-    print(str(i+1) + ". " + lang[i])
+    upperlang.append(lang[i].upper())
+ul2 = upperlang
+upperlang = sorted(upperlang)
+for j in range(len(lang)):
+    print(str(j+1) + ". " + lang[ul2.index(upperlang[j])])
