90fed86a07a75fb97621ccb3ad0a47739f36ef2f
Orionband <98924210+Orionband@users.noreply.github.com>
2025-06-12 09:39:28 -0500

    Create pyramid.d

---
+++ left/pyramid.d
@@ -0,0 +1,12 @@
+import std.stdio;
+import std.conv;
+void main() {
+    write("Number: ");
+    int num = to!int(readln());
+    writeln("\n");
+    string block = "";
+    foreach (i; 0 .. num) {
+        block ~= "*";
+        writeln(block);
+    }
+}
