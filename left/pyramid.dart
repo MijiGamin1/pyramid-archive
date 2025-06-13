d6df0f9324b72a581f9d25374b402753fd036af6
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-06-13 11:33:00 -0500

    Create pyramid.dart

---
+++ left/pyramid.dart
@@ -0,0 +1,11 @@
+//Dart
+import "dart:io";
+void main() {
+  print("enter number:");
+  int num= int.parse(stdin.readLineSync()!);
+  String block = "";
+  for (var i = 0; i < num; i++) {
+      block = block + "*";
+      print(block);
+  }
+}
