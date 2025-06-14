c74343cc7a6a1a705d535d52bc285dad51af0b58
Orionband <98924210+Orionband@users.noreply.github.com>
2025-06-14 15:48:27 -0500

    Create pyramid.ex

---
+++ left/pyramid.ex
@@ -0,0 +1,16 @@
+defmodule Pyramid do
+  def main do
+    IO.puts("Number:")
+    {num, _} = IO.gets("") |> Integer.parse()
+    draw(1, num)
+  end
+
+  defp draw(i, n) when i <= n do
+    IO.puts(String.duplicate("*", i))
+    draw(i + 1, n)
+  end
+
+  defp draw(_, _), do: :ok
+end
+
+Pyramid.main()
