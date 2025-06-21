a0ed0be8e087c23c239bede39c705eded864910d
The Real Mayo <syedfarhanwarsi@gmail.com>
2025-06-20 19:58:34 -0500

    verilog
    verilog just can't take user inputs unless you make a whole data.txt file and make another testbench file to read the txt. i can make that if it is desired.

---
+++ left/Verilog/pyramid.v
@@ -0,0 +1,11 @@
+module pyramid #(parameter HEIGHT = 5) (); //User input here
+  integer i,j;
+  initial begin
+    for (i = 1; i <= HEIGHT; i = i + 1) begin
+        for (j = 1; j <= i; j = j + 1) begin
+            $write("*");
+        end
+        $display("");
+    end
+  end
+endmodule
