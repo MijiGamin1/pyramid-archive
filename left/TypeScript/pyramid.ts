c0d5c7516ee19199f24f4c1fd69702094dc35f65
realmayo1 <syedfarhanwarsi@gmail.com>
2025-06-27 10:03:50 -0500

    SYBAU Marc ong tspmo icl rn fr fr lowk js kys vro

--- left/TypeScript/pyramid.ts
+++ left/TypeScript/pyramid.ts
@@ -1,4 +1,12 @@
-//Type Script => ts => ts pmo icl => icl ts pmo sm n sb rn ngl, r u srsly srs n fr rn vro? Smh lol atp js go 💔... b fr vro, idek nm, brb gng gtg atm lmao, bt ts pmo 2 js lmk lol onb fr, ac nvm b wt istg ts vro keys🙏💔 ts pmo lk sftu fr nc ts is nt tff vro ike wyd 💀 yapping all this w/o a shred of self-awareness 😹💅 ur takes r giving 404 drip not found 🚫🧥 idek if ur trolling or j jus lost rn 😭😭 atp it’s js not adding up 🔢🧠 fr lmk wn u done being chronically loud n emotionally incorrect 💅 cause rn u sounding like a reddit thread written by a microwave 😭📟 bt ts pmo so bad I had to blink twice 💀💀 icl, I’m this 🤏 close to rage quitting reality 😭👋 frfr ts gnna make me go npc mode n js walk into the sea 🌊🚶‍♂️🤖 tl;dr: stg vro icl ts pmo w/ extra lag n delulu 💻🧠 ts is lowkey a PSA 2 log off n touch grass rn 🚨🌱 ts pmo 🗣️ ts pmo 😔 ts pmo 🕊️ 
+//Type Script => ts => ts pmo icl => icl ts pmo sm n sb rn ngl, r u srsly srs n fr rn vro? 
+//Smh lol atp js go 💔... b fr vro, idek nm, brb gng gtg atm lmao, bt ts pmo 2 js lmk lol onb fr, ac nvm b wt istg ts vro keys🙏💔 
+//ts pmo lk sftu fr nc ts is nt tff vro ike wyd 💀 yapping all this w/o a shred of self-awareness 😹💅 
+//ur takes r giving 404 drip not found 🚫🧥 idek if ur trolling or j jus lost rn 😭😭 atp it’s js not adding up 🔢🧠 
+//fr lmk wn u done being chronically loud n emotionally incorrect 💅 cause rn u sounding like a reddit thread written by a microwave 😭📟 
+//bt ts pmo so bad I had to blink twice 💀💀 icl, I’m this 🤏 close to rage quitting reality 😭👋 
+//frfr ts gnna make me go npc mode n js walk into the sea 🌊🚶‍♂️🤖 tl;dr: stg vro icl ts pmo w/ extra lag n delulu 💻🧠 
+//ts is lowkey a PSA 2 log off n touch grass rn 🚨🌱 ts pmo 🗣️ ts pmo 😔 ts pmo 🕊️ 
+//SYBAU Marc 💔💔🥀🥀
 var userInput = parseInt(prompt("Enter the number:"));
 var block ="";
 for (let i = 0; i< userInput; i++) {
