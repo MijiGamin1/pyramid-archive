d38a35fb3046b171c49b40b6fd4d5c94a227fee5
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-07-16 15:05:00 -0500

    Create pyramid.f90

---
+++ left/FORTRAN 90/pyramid.f90
@@ -0,0 +1,14 @@
+program pyramid !originally created and deleted by Orionband
+    implicit none
+    integer :: num, i, j
+    character(len=100) :: block
+    print *, 'Number:'
+    read *, num
+    do i = 1, num
+        block = ''
+        do j = 1, i
+            block(j:j) = '*'
+        end do
+        print *, trim(block(1:i))
+    end do
+end program pyramid
