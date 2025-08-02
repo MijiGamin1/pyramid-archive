384e9920613cbf7d091e0ff09d9911ea637959c2
Ernie <ernie@memum.io>
2025-08-02 08:31:41 -0400

    Wait we have `for`-loops???
    New to Lean 4, apparently. You get them inside `do`-blocks

--- left/Lean 4/pyramid.lean
+++ left/Lean 4/pyramid.lean
@@ -1,20 +1,9 @@
-def pyramid (n : Nat): List String :=
-  List.reverse $ descending_pyramid n
-  where
-    stars s := String.mk $ List.replicate s '*'
-    descending_pyramid : Nat → List String
-      | 0 => []
-      | n'+1 => (stars $ n'+1) :: descending_pyramid n'
-
-def printlns : List String → IO Unit
-  | [] => pure ()
-  | line :: rest => do
-      IO.println line
-      printlns rest
-
 def main : IO Unit := do
-    let stdin ← IO.getStdin
     IO.print "Height: "
-    let line ← stdin.getLine
+
+    let line ← (← IO.getStdin).getLine
     let height := line |> String.trim |> String.toNat!
-    printlns $ pyramid height
+
+    for i in [1:height+1] do
+      let stars := String.mk $ List.replicate i '*'
+      IO.println stars
