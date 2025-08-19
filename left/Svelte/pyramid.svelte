6371d3f89c57423d6aaf0f98dad5a6bb0b9f56e0
MijiGamin1 <108433802+MijiGamin1@users.noreply.github.com>
2025-08-19 14:32:22 -0500

    lowk might be cheating but i feel it's different enough to which it's fine

---
+++ left/Svelte/pyramid.svelte
@@ -0,0 +1,11 @@
+<script>
+	let n = $state('');
+</script>
+<input bind:value={n}/> <!-- ran in svelte.dev/playground/ -->
+<br>
+{#each {length: n} as _, i}
+    {#each {length: i+1} as _, j}
+				*
+		{/each}
+		<br>
+{/each}
